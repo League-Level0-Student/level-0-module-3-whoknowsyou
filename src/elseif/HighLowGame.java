60507a83fe050b217e8f823dd3d63879056e2aae
Jack <jpstevenson.avi@gmail.com>
2017-11-21 17:54:23 -0800

    highlow started fizzbuzz

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -11,25 +11,34 @@ public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int random = new Random().nextInt(101);
 		// 2. Print out the random variable above
-		println.random
 		// 11. do the following 10 times
-
+for(int j=0;j<=10;j++){
 			// 1. ask the user for a guess using a pop-up window, and save their response 
-		JOptionPane.showMessageDialog(null, "guess a nummber between 1-5");
+		String cats=JOptionPane.showInputDialog(null, "guess a nummber between 1-100");
 			// 4. convert the users’ answer to an int (Integer.parseInt(string))
 		int cat =Integer.parseInt(cats);		
 			// 5. if the guess is correct
-				// 6. win
+		if (cat==random) {
+		// 6. win
+		System.out.println("you win");
+		}else
 			// 7. if the guess is high
+		if (cat>random) {
+		System.out.println("to high");	
+		}else
 				// 8. tell them it's too high
 			// 9. if the guess is low
+		if (cat<random) {
+System.out.println("to low");			
+		}else{
 				// 10. tell them it's too low
-
-		// 12. tell them they lose
+System.out.println("you lose");
+	// 12. tell them they lose
 	}
-
+		}
+	
 }
-
+}
 
--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -26,6 +26,21 @@ package modulo;
 **/
 
 public class FizzBuzz {
+public static void main(String[] args) {
+for (int i = 20; i >=0; i--) {
+	
+if (i%3&&5==0) {
+System.out.println("fizbuzz");	
 
+}elseif (i%3==0) {
+System.out.println("fizz");	
+}else if (i%5==0) {
+System.out.println("buzz");	
+	
+	
+}else{	
+System.out.println(i);	
+}	
 }
+}}
 
